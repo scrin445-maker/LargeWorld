147fdfb37a93617bbbacacbd8fe21aac98836157
Dylan Lu <scrin445.dl@gmail.com>
2026-04-16 18:02:59 +0800

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,11 @@
+# 1. Unpack resourcepacks if .zip files
+# 2. Create output resourcepack
+# 3. Scan resourcepacks for textures
+# 3.5. Ignore certain textures based on an overrides.json
+# 4. Create folders for each textures in output resourcepack
+# 5. Splice textures into 4 tiles
+# 6. Store in matching folder in output
+# 6.5. Group folders together based on a group.json file
+
+if __name__ == "__main__":
+    pass
