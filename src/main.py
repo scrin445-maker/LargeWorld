124d2cd76fbed12aa3caef2c13257ee959483430
Dylan Lu <scrin445.dl@gmail.com>
2026-05-03 19:36:43 +0800

    output resourcepack successfully generated

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import os
 from PIL import Image
+import shutil
 
 # 1. Unpack resourcepacks if .zip files
 # 2. Create output resourcepack
@@ -13,27 +14,32 @@ from PIL import Image
 
 
 if __name__ == "__main__":
-    
-    # Walk input folder for resourcepacks
-    for root, dirs, files in os.walk("./src/input"):
-        
-        # Look for textures
-        for infile in files:
-            if infile.endswith(".png") and "white" in infile:
-                
-                texture_name = infile.replace(".png", "")
-                texture = Image.open(os.path.join(root, infile))
-                texturemap = []
 
-                # Crop starting from the upper left
-                for y in range(2):
-                    for x in range(2):
-                        box = [x * 8, y * 8, 8 * (x + 1), 8 * (y + 1)]
-                        texturemap.append(texture.crop(box))
-                
-                # Continuity starts from the bottom left so name tiles appropriately        
-                texturemap[2].save("0.png")
-                texturemap[3].save("1.png")
-                texturemap[0].save("2.png")
-                texturemap[1].save("3.png")
+    # Create output resourcepack
+    for pack in os.listdir("./src/input"):
+        output = "./src/output/{}/assets/optifine/ctm/".format(pack)
+        os.makedirs(output, exist_ok=True)
+    
+        # Walk input folder for resourcepacks
+        for root, dirs, files in os.walk("./src/input/{}".format(pack)):
+            
+            # Look for textures
+            for infile in files:
+                if infile.endswith(".png") and "white" in infile:
+                    
+                    texture_name = infile.replace(".png", "")
+                    texture = Image.open(os.path.join(root, infile))
+                    texturemap = []
+
+                    # Crop starting from the upper left
+                    for y in range(2):
+                        for x in range(2):
+                            box = [x * 8, y * 8, 8 * (x + 1), 8 * (y + 1)]
+                            texturemap.append(texture.crop(box))
+                    
+                    # Continuity starts from the bottom left so name tiles appropriately        
+                    texturemap[2].save("0.png")
+                    texturemap[3].save("1.png")
+                    texturemap[0].save("2.png")
+                    texturemap[1].save("3.png")
 
