7c56a005ca116c7cd05767f3ffcb69692d02e77b
Dylan Lu <scrin445.dl@gmail.com>
2026-07-30 17:36:29 +0800

    Modularised code.
    (cherry picked from commit 3e000c73f3c2b3a994c9f36820c70e454e2dd9a6)

--- src/main.py
+++ src/main.py
@@ -10,30 +10,72 @@ from PIL import Image
 # 6. Store in matching folder in output
 # 6.5. Group folders together based on a group.json file
 
+INPUT_DIR = './src/input'
+OUTPUT_DIR = './src/output'
+
+
+def getResourcepacks(inputDir):
+    resourcepacks = []
+    for pack in os.listdir(inputDir):
+        resourcepacks.append(pack)
+        print("Resourcepack", pack, "found.", sep=" ")
+    return resourcepacks
+
+
+def getTextures(resourcepack, inputDir, outputDir):
+    textures = []
+
+    pack = inputDir + '/' + resourcepack
+    
+    for root, dirs, files in os.walk(pack):
+        for infile in files:
+            if str(infile).endswith(".png"):
+                textures.append(infile)
+    
+    return textures
+
+
+def generateContinuity(resourcepack, inputDir, outputDir):
+    
+    # Create output dir
+    try:
+        os.makedirs(outputDir, exist_ok=False)
+        print("Created ./src/output folder.")
+    except OSError:
+        print("./src/output folder already exists.")
+
+    textures = getTextures(resourcepack, inputDir, outputDir)
+    rootDir = "{}/{}/assets/minecraft/textures/block".format(inputDir, resourcepack)
+    outputResourcepack = "{}/{}/assets/minecraft/optifine/ctm/".format(outputDir, resourcepack)
+    
+    for texture in textures:
+        texture_name = texture.replace(".png", "")
+        texture_file = Image.open(os.path.join(rootDir, texture))
+        texture_map = []
+        out_folder = outputResourcepack + texture_name
+        
+        os.makedirs(out_folder, exist_ok=True)
+        
+        for y in range(2):
+            for x in range(2):
+                box = [x * 8, y * 8, 8 * (x + 1), 8 * (y + 1)]
+                texture_map.append(texture_file.crop(box))
+        
+        # Continuity starts from the bottom left so name tiles appropriately        
+        texture_map[2].save(out_folder + "/0.png")
+        texture_map[3].save(out_folder + "/1.png")
+        texture_map[0].save(out_folder + "/2.png")
+        texture_map[1].save(out_folder + "/3.png")
+        
+        with open(out_folder + "/{}.properties".format(texture_name), "w") as f:
+            f.write("matchTiles={}\nmethod=repeat\nwidth=2\nheight=2\ntiles=0-3\n".format(texture_name))
+            f.close()
+        
+    return
 
 
 if __name__ == "__main__":
-    
-    # Walk input folder for resourcepacks
-    for root, dirs, files in os.walk("./src/input"):
-        
-        # Look for textures
-        for infile in files:
-            if infile.endswith(".png") and "white" in infile:
-                
-                texture_name = infile.replace(".png", "")
-                texture = Image.open(os.path.join(root, infile))
-                texturemap = []
 
-                # Crop starting from the upper left
-                for y in range(2):
-                    for x in range(2):
-                        box = [x * 8, y * 8, 8 * (x + 1), 8 * (y + 1)]
-                        texturemap.append(texture.crop(box))
-                
-                # Continuity starts from the bottom left so name tiles appropriately        
-                texturemap[2].save("0.png")
-                texturemap[3].save("1.png")
-                texturemap[0].save("2.png")
-                texturemap[1].save("3.png")
-
+    resourcepacks = getResourcepacks(INPUT_DIR)
+    for resourcepack in resourcepacks:
+        test = generateContinuity(resourcepack, INPUT_DIR, OUTPUT_DIR)
